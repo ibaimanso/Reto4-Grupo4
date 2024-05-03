15cb812453769c75407143b1e6c5b54eb91e1326
KaietZarGo <kaiet.zarzosago@elorrieta-errekamari.com>
2024-05-03 17:36:12 +0200

    test scrollpane

--- ReproductorDeMusica/src/modeloPaneles/DescubrirMusica.java
+++ ReproductorDeMusica/src/modeloPaneles/DescubrirMusica.java
@@ -1,18 +1,25 @@
 package modeloPaneles;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-import javax.swing.DefaultListModel;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.border.LineBorder;
 
 import logica.GestionDeLaInformacion;
 import modelo.Musico;
 import view.VistaPrincipal;
-import java.awt.Dimension;
 
 public class DescubrirMusica extends JPanel {
 	
@@ -24,32 +31,59 @@ public class DescubrirMusica extends JPanel {
 		//musicos = gestion.devolverMusicos();
 		
 		setSize(new Dimension(704, 603));
-//		JPanel panel = new JPanel();
-//		panel.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
-//		panel.setBackground(new Color(255, 255, 255));
-//		panel.setBounds(34, 65, 568, 133);
-//		add(panel);
-//		panel.setLayout(null);
 		
-		JList<JPanel> list = new JList<JPanel>();
-		JScrollPane scrollArtista = new JScrollPane(list);
+		/**
+		 *  Crear un panel para contener los JLabels
+		 */
+		JPanel panel = new JPanel();
+		//panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.setLayout(new GridLayout(0,1));
+		
+		/**
+		 *  Agregar JLabels al panel
+		 */
+		for (int i = 0; i < 40; i++) {
+			JPanel panelItem = new JPanel();
+			panelItem.setLayout(new GridLayout());
+			panelItem.setSize(80, 396); 
+			
+			// Cargar imagen
+//			ImageIcon imageIcon = new ImageIcon("imagenes/cd.jpg");
+//			Image image = imageIcon.getImage();
+//			Image newImage = image.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+//			ImageIcon newImageIcon = new ImageIcon(newImage);
+//			JLabel imageLabel = new JLabel(newImageIcon);
+//			panelItem.add(imageLabel);
+
+			// Agregar JLabels debajo de la imagen
+			JLabel label1 = new JLabel("Nombre: "+i);
+			label1.setSize(80,396);;
+			panelItem.add(label1);
+			//Le damos nombre para identificarlo
+			panelItem.setName("panel " + i);
+			// Añadir un borde al panelItem
+			panelItem.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			
+			
+			// Añadir escuchador al panel
+			panelItem.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			JPanel clickedPanel = (JPanel) e.getSource();
+			JOptionPane.showMessageDialog(null, "Has hecho clic en: " + clickedPanel.getName()+"que tiene los labels:"+((JLabel)clickedPanel.getComponent(0)).getText());
+
+			}
+			});
+			// Agregar panelItem al panel principal
+			panel.add(panelItem);
+			}
+			
+		
+		JScrollPane scrollArtista = new JScrollPane(panel);
+		scrollArtista.getVerticalScrollBar().setUnitIncrement(30);
 		scrollArtista.setSize(445, 396);
 		scrollArtista.setLocation(34, 65);
-		//scrollArtista.setLayout(null);
 		add(scrollArtista);
-		DefaultListModel<JPanel> panelImagenes = new DefaultListModel<JPanel>();
-		
-		for (int i = 0; i < 7; i++) {
-			JPanel panelArtista = new JPanel();
-			panelArtista.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
-			panelArtista.setBackground(new Color(255, 255, 255));
-			//panelArtista.setBounds(34, 65, 568, 133);
-			panelArtista.setSize(150, 396);
-			panelArtista.setLayout(null);
-			panelImagenes.add(i, panelArtista);
-		}
-
-		list.setModel(panelImagenes);
 
 
 	}
